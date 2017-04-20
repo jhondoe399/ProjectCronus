5c8e671bd284ca4161d2c81fb5ae4563d246da48
jhondoe399 <jhondoe399@gmail.com>
2017-04-20 18:21:24 +0300

    Added Clients Upload

--- AddClient.aspx
+++ AddClient.aspx
@@ -4,7 +4,40 @@
     <asp:SqlDataSource ID="SqlDataSourceAddClient" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Clients]">
     </asp:SqlDataSource>
 
+    <div id="PageResponse" runat="server"></div>
+    <div class="table_container">
+        <div class="cell_left">
+            Client name:
+        </div>
+        <div class="cell_right">
+            <asp:TextBox ID="TextBoxClientName" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFildValidatorNumeClient" runat="server" ErrorMessage="Incorrect name" controlToValidate="TextBoxClientName" ></asp:RequiredFieldValidator>
+        </div>
+        <div class="cell_left">
+            Client Adress:
+        </div>
+        <div class="cell_right">
+            <asp:TextBox ID="TextBoxAdress" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidatorAdress" runat="server" ErrorMessage="Incorrect adress" ControlToValidate="TextBoxAdress"></asp:RequiredFieldValidator>
+        </div>
+        <div class="cell_left">
+            Client CUI:
+        </div>
+        <div class="cell_right">
+            <asp:TextBox ID="TextBoxCUI" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidatorCUI" runat="server" ErrorMessage="Incorrect CUI" ControlToValidate="TextBoxCUI"></asp:RequiredFieldValidator>
+        </div>
+        <div class="cell_left">
+            Client Image:
+        </div>
+        <div class="cell_right">
+            <asp:FileUpload ID="FileUploadImage" runat="server" />
+        </div>
+        <div>
+            <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" OnClick="Submit"/>
+        </div>
 
+    </div>
 
 
 </asp:Content>
--- AddClient.aspx.cs
+++ AddClient.aspx.cs
@@ -4,11 +4,53 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.IO;
 
 public partial class AddClient : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Submit(object sender, EventArgs e)
+    {
+
+        String name, address, cui, sql;
+        try
+        {
+            name = TextBoxClientName.Text;
+            address = TextBoxAdress.Text;
+            cui = TextBoxCUI.Text;
+
+            if (FileUploadImage.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUploadImage.PostedFile.FileName);
+                string fileExtension = Path.GetExtension(FileUploadImage.PostedFile.FileName);
+                FileUploadImage.PostedFile.SaveAs(Server.MapPath("~/uploads/clients/") + name.ToLower() + fileExtension);
+            }
+
+            sql = "INSERT INTO Clients (name, address, cui) VALUES (@name, @address, @cui)";
+            SqlConnection sqlconnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Project\App_Data\Database.mdf;Integrated Security=True");
+            sqlconnection.Open();
+            SqlCommand sqlcomm = new SqlCommand(sql, sqlconnection);
+            sqlcomm.Parameters.AddWithValue("name", name);
+            sqlcomm.Parameters.AddWithValue("address", address);
+            sqlcomm.Parameters.AddWithValue("cui", cui);
+            sqlcomm.ExecuteNonQuery();
+            sqlconnection.Close();
+            TextBoxClientName.Text = "";
+            TextBoxAdress.Text = "";
+            TextBoxCUI.Text = "";
+            PageResponse.InnerHtml = "Client added";
+
+        }
+
+        catch (Exception ex)
+        {
+            PageResponse.InnerHtml = "Something went wrong." + ex.Message + "<br>" + ex.Source;
+        }
+        
     }
 }
